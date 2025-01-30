9a7761cd3f79ccac3d933471d8b853a1e6804372
voisnoa <markzlopp@gmail.com>
2025-01-30 10:05:40 +0530

    set up DB and connected to express server

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
-const port = 3010;
+
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+console.log(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(err => console.error('Database connection error:', err));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+  });
